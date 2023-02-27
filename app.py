9fdbcf6787b3195f0776578c8091b2580cb9eb30
Sudheesh Kumar <103947453+Sudheesh2609@users.noreply.github.com>
2023-02-27 19:44:32 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -1,3 +1,3 @@
 import streamlit as st
 
-st.header("Hello I am Sudheesh Kumar Potla enrolled in Data Science internship program from Innomatics Research Lab")
+st.header("Hello I am Sudheesh Kumar Potla enrolled in Data Science internship program ")
